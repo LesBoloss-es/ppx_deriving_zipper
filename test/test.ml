7aece6d3379112972b0453fd6a6d3c1bf97bc2a8
Niols <niols@niols.fr>
2019-05-01 12:07:04 +0200

    improve test

--- test/test.ml
+++ test/test.ml
@@ -1,5 +1,35 @@
 type tree =
   | Nil
-  | Unode of float * tree
+  | UNode of float * tree
   | BNode of int * tree * tree
 [@@deriving zipper]
+
+let a =
+  BNode (
+    7,
+    UNode (
+      2.5,
+      UNode (
+        7.8,
+        Nil
+      )
+    ),
+    BNode (
+      4,
+      Nil,
+      UNode (
+        2.7,
+        BNode (
+          89,
+          Nil,
+          Nil
+        )
+      )
+    )
+  )
+
+let za = zip_tree a
+
+let () = assert (za = (a, []))
+
+let () = print_endline "test.ml OK"
