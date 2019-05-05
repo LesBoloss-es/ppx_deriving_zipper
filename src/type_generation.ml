5a5b2e07ca56886719994d5d8a4beded0c596e6f
Martin Pépin <kerl@wkerl.me>
2019-05-05 23:25:16 +0200

    Fix zipper generation

--- src/type_generation.ml
+++ src/type_generation.ml
@@ -17,8 +17,17 @@ let subst t = function
 let guess_ancestor_name name = name ^ "_ancestor"
 let guess_zipper_name name = name ^ "_zipper"
 
-let all {name; def} =
+let ancestor {name; def} =
   let d = Derive.typ name def in
-  let ancestor = {name = guess_ancestor_name name; def = subst unit d} in
-  let zipper = {name = guess_zipper_name name; def = assert false} in
+  {name = guess_ancestor_name name; def = subst unit d}
+
+let zipper type_name ancestor_name =
+  let name = guess_zipper_name type_name in
+  let ancestors = Constr ("list", [Constr (ancestor_name, [])]) in
+  let typ = Product [Constr (type_name, []); ancestors] in
+  {name; def = Flat typ}
+
+let all typ =
+  let ancestor = ancestor typ in
+  let zipper = zipper typ.name ancestor.name in
   ancestor, zipper
