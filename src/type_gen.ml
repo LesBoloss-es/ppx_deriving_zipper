4edd953077255dfd120067941a2856a5c2bf0053
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2022-04-21 17:40:50 +0200

    Also generate *_poly_zdvars

--- src/type_gen.ml
+++ src/type_gen.ml
@@ -8,8 +8,19 @@ let type_gen (td : Types.decl) : Syntax.type_declaration list =
       ; definition= Variant (Types.Print.polynomial poly')
       ; loc= Location.none }
   in
+  let poly_zdvars =
+    List.map
+      (fun var ->
+         let poly' = Derive.polynomial var poly in
+         Syntax.
+           { name= Naming.poly_zd td.name var
+           ; vars= td.vars
+           ; definition= Variant (Types.Print.polynomial poly')
+           ; loc= Location.none })
+    td.vars
+  in
   [poly_zdfix]
-  (* let poly_zdvars = assert false in *)
+  @ poly_zdvars
   (* let zdz = assert false in *)
   (* let dis = assert false in *)
   (* let ancestor = assert false in *)
