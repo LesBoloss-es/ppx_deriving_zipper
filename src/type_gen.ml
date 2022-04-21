8d387dfa862cba4df221d94d12f1e2d64034348f
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2022-04-21 18:12:59 +0200

    Refactor type_gen
    replace `by` by a more general helper

--- src/type_gen.ml
+++ src/type_gen.ml
@@ -1,12 +1,15 @@
 let type_gen (td : Types.decl) : Syntax.type_declaration list =
   let (Fixpoint (poly, fix_var)) = td.def in
-  let by =
-    Types.App (td.name, List.map Types.var_ td.vars)
+
+  let app_to_vars name =
+    Types.App (name, List.map Types.var_ td.vars)
   in
 
   let poly_zdfix =
     let poly' = Derive.polynomial fix_var poly in
-    let poly' = Types.substitute_polynomial ~var:fix_var ~by poly' in
+    let poly' = Types.substitute_polynomial poly'
+        ~var:fix_var ~by:(app_to_vars td.name)
+    in
     Syntax.
       { name= Naming.poly_zdfix td.name
       ; vars= td.vars
@@ -17,13 +20,16 @@ let type_gen (td : Types.decl) : Syntax.type_declaration list =
     List.map
       (fun var ->
          let poly' = Derive.polynomial var poly in
-         let poly' = Types.substitute_polynomial ~var:fix_var ~by poly' in
+         let poly' = Types.substitute_polynomial poly'
+             ~var:fix_var ~by:(app_to_vars td.name)
+         in
          Syntax.
            { name= Naming.poly_zd td.name var
            ; vars= td.vars
            ; definition= Variant (Types.Print.polynomial poly')
            ; loc= Location.none })
-    td.vars
+      td.vars
+  in
   in
 
   [poly_zdfix]
