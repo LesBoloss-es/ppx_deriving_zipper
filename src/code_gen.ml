89894db22aa0c7e8208d3ecbc40240d047978f55
Martin Pépin <kerl@wkerl.me>
2022-06-03 16:19:57 +0200

    Add some fixmes

--- src/code_gen.ml
+++ src/code_gen.ml
@@ -5,9 +5,13 @@ open More_ast_helpers
 (** Given a type [foo], generate a function of type [foo -> foo_zipper] that
   constructs the zipper pointing at the root of the structure. *)
 let zip (td : Types.decl) : _ =
+  (* FIXME. Use proper locations *)
   let loc = Location.none in
+  (* FIXME. We probably want a helper doing this *)
   let type_variables = List.map (Typ.var ~loc) td.vars in
+  (* FIXME. We probably want a helper doing this *)
   let type_annotation = Typ.constr ~loc (lid td.name) type_variables in
+  (* FIXME. We probably want a helper doing this *)
   let zipper_annotation =
     Typ.constr ~loc (lid (Naming.zdz td.name)) type_variables
   in
