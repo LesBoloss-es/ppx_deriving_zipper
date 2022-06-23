2669570de5ced9bf9f73e695a3ef96631692d5aa
Niols <niols@niols.fr>
2022-06-23 20:45:03 +0200

    A lot of crap; preparing for BeAuTy

--- examples/tree_list.ml
+++ examples/tree_list.ml
@@ -91,3 +91,166 @@ module Zipper = struct
       | Top trees -> Up (Raw (Node (tree, trees)), ancestors)
       | Up list_zipper -> Up (InNode1 (tree, list_zipper), ancestors)
 end
+
+
+(** {2 Examples} *)
+
+(** {3 How to Rewrite Recursive Algorithms for Dummies} *)
+
+let exists path tree =
+
+  let rec exists (path : int list) (tree : Zipper.t) =
+    match path with
+    | [] -> true
+    | n :: path ->
+      assert (n >= 0);
+      match Zipper.view tree with
+      | Leaf -> false
+      | Node (tree, _) when n = 0 -> exists path (tree ())
+      | Node (_, trees) -> exists_list (n - 1) path (trees ())
+
+  and exists_list (n : int) (path : int list) (list : List2.Zipper.t) =
+    assert (n >= 0);
+    match List2.Zipper.view list with
+    | [] -> false
+    | tree :: _ when n = 0 -> exists path (tree ())
+    | _ :: trees -> exists_list (n - 1) path (trees ())
+
+  in
+
+  if List.exists ((>) 0) path then
+    invalid_arg "Tree_list.exists";
+
+  exists path (Zipper.zip tree)
+
+(** {3 Move in Four Directions}
+
+   - We always point on a constructor of [tree].
+
+ *)
+
+type direction = Up | Left | Right | Down
+type path = direction list
+
+let rec big_up (zipper : Zipper.t) : Zipper.go_up_result =
+  match Zipper.go_up zipper with
+  | (Top _ | Up (Raw _, _)) as result -> result
+  | Up ((InNode1 _, _) as zipper) -> big_up zipper
+  (* | Up zipper -> big_up zipper *)
+
+
+
+
+
+
+
+let view ((head, ancestors) : Zipper.t) : Zipper.view =
+  match head with
+  | Raw Leaf -> Leaf
+  | Raw (Node (t, ts)) ->
+    Node ((fun () -> (Raw t, Node0 (Hole, ts, ancestors))),
+          (fun () -> (InNode1 (t, List2.Zipper.zip ts, ancestors))))
+  | InNode1 (t, l_zipper) ->
+    let (ts, l_ancestors) = l_zipper in
+    match ts with
+    | [] -> Nil
+    | t' :: ts' ->
+      Cons ((fun () -> (Raw t', Node1 (t, Cons0 (Hole, ts', l_ancestors), ancestors))),
+            (fun () -> (InNode1 (t, (ts', Cons1 (t', Hole, l_ancestors))), ancestors)))
+
+
+
+
+
+let move_once (dir : direction) (tree : Zipper.t) : Zipper.t option =
+  match dir with
+  | Down ->
+    (
+      match Zipper.view tree with
+      | Leaf -> None
+      | Node (tree, _) -> Some (tree ())
+      | _ -> assert false
+    )
+  | Up ->
+    (
+      match big_up tree with
+      | Top _ -> None
+      | Up tree -> Some tree
+    )
+  | Left ->
+    (
+      match Zipper.up_view tree with
+      | Top -> None
+      | Node (Here _, _) -> None
+      | Cons (Here up, _) ->
+        (
+          match Zipper.up_view (up ()) with
+          | Node (NotHere tree, Here _)
+          | Cons (NotHere tree, Here _) -> Some (tree ())
+          | _ -> assert false
+        )
+      | _ -> assert false
+    )
+  | Right ->
+    (
+      match Zipper.up_view tree with
+      | Top -> None
+      | Node (Here _, NotHere trees)
+      | Cons (Here _, NotHere trees) ->
+        (
+          match Zipper.view (trees ()) with
+          | Nil -> None
+          | Cons (tree, _) -> Some (tree ())
+          | _ -> assert false
+        )
+      | _ -> assert false
+    )
+
+
+
+
+
+type tree_zipper_view =
+  | Leaf
+  | Node of tree_zipper thunk * tree_zipper thunk
+  | InNode of view_arg * list_zipper_view view_arg
+
+
+
+
+
+
+          match List2.Zipper.up_view (list ()) with
+          | Top  ->
+            (
+              (* NOTE: maybe the above [Node] could carry a [NotHere thunk]. *)
+              match Zipper.view (up ()) with
+              | Node (tree, _) -> Some (tree ())
+              | _ -> assert false
+            )
+          | Cons (Here _, _) -> assert false
+          | Cons (_, Here up_list) ->
+            (
+              match List2.Zipper.view (up_list ()) with
+              | Nil -> assert false
+              | Cons (tree, trees) ->
+                (Raw tree, Cons0 (Hole, (fst (trees ())), ancestors))
+
+
+
+        )
+    )
+
+
+
+
+
+
+
+
+
+
+let rec move path tree =
+  match path with
+  | [] -> Some tree
+  | dir :: path -> Option.bind (move_once dir tree) (move path)
