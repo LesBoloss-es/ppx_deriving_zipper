0bd2d57631de2d83a7d13862cc03217bbef86abc
Martin Pépin <kerl@wkerl.me>
2022-11-25 16:03:38 +0100

    Cleanup

--- src/ppx_deriving_zipper.ml
+++ src/ppx_deriving_zipper.ml
@@ -1,24 +1,3 @@
-(* open Ast_helper *)
-(* open Parsetree *)
-
-(* let wrap_decl ?(non_rec=true) ~is_derivative d = *)
-(*   Str.type_ *)
-(*     Asttypes.(if non_rec then Nonrecursive else Recursive) *)
-(*     [Ztype.to_decl ~is_derivative d] *)
-
-(* let mangle_type_decl_to_module ?(fixpoint="t") affix type_ = *)
-(*   let cap = String.capitalize_ascii in *)
-(*   (match affix with *)
-(*    | `Prefix prefix | `PrefixSuffix (prefix, _) -> cap prefix *)
-(*    | _ -> "") *)
-(*   ^ *)
-(*   (let type_ = type_.ptype_name.txt in *)
-(*    if type_ = fixpoint then "" else cap type_) *)
-(*   ^ *)
-(*   (match affix with *)
-(*    | `Suffix suffix | `PrefixSuffix (_, suffix) -> cap suffix *)
-(*    | _ -> "") *)
-
 let handle_type_decl type_decl =
   Format.eprintf "%a\n;;@." Pprintast.structure_item
     (Ast_helper.Str.type_ Asttypes.Recursive [type_decl]);
@@ -35,55 +14,10 @@ let handle_type_decl type_decl =
   List.iter (Pprintast.structure_item Format.err_formatter) stris;
   exit 0
 
-(* let Types.Fixpoint (p, fix_var) = decl.def in *)
-(* List.iter *)
-(*   (fun x -> *)
-(*      let p' = Derive.polynomial x p in *)
-(*      let decl_lol = *)
-(*        Syntax.{ name = "derivative_"^x; vars = []; loc = Location.none; *)
-(*                 definition = Variant (Types.Print.polynomial p') } *)
-(*        |> Syntax.Print.type_declaration *)
-(*      in *)
-(*      Format.eprintf "%a\n;;\n%a\n;;@." *)
-(*        Types.pp_polynomial p' *)
-(*        Pprintast.structure_item (Ast_helper.Str.type_ Asttypes.Nonrecursive [decl_lol]) *)
-(*   ) *)
-(*   (fix_var :: decl.vars) *)
-
-let type_decl_str ~options ~path =
-  ignore options;
-  ignore path;
-  function [type_decl] -> handle_type_decl type_decl | _ -> assert false
-
-(*   (\* sanity checks / debugging *\) *)
-(*   assert (List.length type_decl.ptype_cstrs = 0); *)
-(*   (match type_decl.ptype_manifest with *)
-(*    | None -> () *)
-(*    | Some core_type -> print_endline (Ppx_deriving.string_of_core_type core_type)); *)
-
-(*   let decl = Ztype.decl_of_type_declaration type_decl in *)
-(*   let derivative, ancestor, zipper = Type_gen.all decl in *)
-(*   let view = Type_gen.view decl in *)
-(*   let thunk = *)
-(*     let loc = Location.none in *)
-(*     [%stri type 'a thunk = unit -> 'a] *)
-(*   in *)
-(*   let in_module name str = Str.module_ (Mb.mk name (Mod.structure str)) in *)
-(*   [ *)
-(*     in_module *)
-(*       (Location.mknoloc (mangle_type_decl_to_module (`Suffix "zipper") type_decl)) *)
-(*       [ *)
-(*         thunk; *)
-(*         wrap_decl ~is_derivative:true derivative; *)
-(*         wrap_decl ~is_derivative:false ancestor; *)
-(*         wrap_decl ~is_derivative:false zipper; *)
-(*         wrap_decl ~is_derivative:false view; *)
-(*         Code_gen.zip decl; *)
-(*         Code_gen.go_up decl derivative; *)
-(*         Code_gen.unzip decl; *)
-(*         Code_gen.view decl; *)
-(*       ] *)
-(*   ] *)
-(* | _ -> assert false *)
+let type_decl_str ~options:_ ~path:_ =
+  function
+    (* FIXME: handle several type declarations (mutually rec types?) *)
+    | [type_decl] -> handle_type_decl type_decl
+    | _ -> assert false
 
 let () = Ppx_deriving.(register (create ~type_decl_str "zipper" ()))
